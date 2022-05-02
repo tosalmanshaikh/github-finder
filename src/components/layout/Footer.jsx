bb34014d5144e0af020a7047a1a4f5df4d7a83a4
Salman <89381137+tosalmanshaikh@users.noreply.github.com>
2022-05-02 22:16:27 +0200

    profile

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
 
-const footerYear = new Date(). getFullYear()
+  const footerYear = new Date().getFullYear()
 
   return (
     <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+
+
+function Home() {
+  return (
+    <>
+        {/* //search component */}
+        <h1 className='text-6xl'><UserResults /></h1>
+    </>
+  )
+}
+
+export default Home
