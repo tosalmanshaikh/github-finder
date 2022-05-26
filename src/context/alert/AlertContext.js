70c9788a2172ef39788bf5fdffba56648f85a5d5
Salman <89381137+tosalmanshaikh@users.noreply.github.com>
2022-05-26 21:31:38 +0200

    ready to deploy

---
+++ src/context/alert/AlertContext.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from "react";
+import alertReducer from "./AlertReducer";
+
+const AlertContext = createContext();
+
+export const AlertProvider = ({ children }) => {
+   const intialState = null;
+
+   const [state, dispatch] = useReducer(alertReducer, intialState);
+
+   // Set an alert
+   const setAlert = (msg, type) => {
+      dispatch({
+         type: "SET_ALERT",
+         payload: { msg, type },
+      });
+
+      setTimeout(() => dispatch({ type: "REMOVE_ALERT" }), 3000);
+   };
+
+   return (
+      <AlertContext.Provider value={{ alert: state, setAlert }}>
+         {children}
+      </AlertContext.Provider>
+   );
+};
+
+export default AlertContext;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import UserResults from '../components/users/UserResults'
+import UserSearch from '../components/users/UserSearch'
 
 
 function Home() {
   return (
     <>
-        {/* //search component */}
+        <UserSearch />
         <h1 className='text-6xl'><UserResults /></h1>
     </>
   )
